054a6f76150dc86ec087136031f3176605684e37
sys-win11 <purep@qq.com>
2024-03-01 15:41:54 +0800

    继续理解move，为什么move实现这么简单，作用却很大？解释为什么a = std::move(b)之后，b可能会用不了了？

--- Items/Item23.cpp
+++ Items/Item23.cpp
@@ -12,8 +12,55 @@
 // typename会将x直接定义为类型，上面的例子也就没有了乘法的可能
 //
 #include <type_traits>
+#include <algorithm>
+#include <iostream>
 
-using namespace std;
+using std::cout, std::remove_reference, std::endl;
+
+class Array
+{
+public:
+    int size_;
+    int *data_;
+
+    explicit Array(int size) : size_(size), data_(new int[size]) {}
+
+    Array(Array &other) : size_(other.size_), data_(new int[size_])
+    {
+        for (int i = 0; i < size_; ++i)
+        {
+            data_[i] = other.data_[i];
+        }
+    }
+
+    Array(Array &&other)  noexcept : size_(other.size_), data_(other.data_)
+    {
+        // 因为现在这个对象指向了other原来内存的内容，如果不清除other的指针，那么other析构时，会把data_也清除
+        // 因为是std::move，返回右值引用
+        other.data_ = nullptr;
+    }
+
+    virtual ~Array()
+    {
+        delete[] data_;
+    }
+};
+
+// 为什么a = std::move(b)之后，b可能会用不了了？
+// std::move只是把左值转换成右值，比如a = std::move(b)，则是相当于a = 右值b
+// 然后因为等于号相当于.operator=()，所以就变为了a.operator=(右值b)
+// =号是用来调用类型的赋值函数的，而如果=的右边是右值，则会调用对应的移动赋值函数
+// 如果对应的移动赋值函数中，将原有的内容删除，则b就用不了了，但是这需要你自己决定
+// 通常我们使用move就是为了这样，所以一般我们也这样实现，所以不是move做了一切，而是我们做了一切
+
+int main()
+{
+    Array b(100000);
+    auto a = std::move(b);
+    cout << a.data_ << endl;
+    cout << b.data_ << endl;
+    return 0;
+}
 
 // std::move无条件的将它的实参转换为右值
 template<typename T>//在std命名空间
@@ -23,10 +70,4 @@ move(T &&param)
     using ReturnType = typename remove_reference<T>::type &&;
     // 返回时将传入的通用引用参数变为右值
     return static_cast<ReturnType>(param);
-}
-
-int main()
-{
-
-    return 0;
 }
